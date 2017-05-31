e31d8e4b26da69d51f8ccadefb61cfa3cad76cb1
AlexeevMikhail <alexeev.michel@gmail.com>
2017-05-31 06:10:52 +0300

    Tests for sort

---
+++ Test/sort_tests.py
@@ -0,0 +1,32 @@
+import subprocess, unittest, os
+from shutil import copyfile
+
+def runTest(num):
+      copyfile('input_test' + num + '.txt', 'input.txt')
+      subprocess.call('./Sort', timeout=10)
+      with open ('output.txt', 'r') as output_file:
+          result=output_file.readlines()
+      with open ('output_test' + num +'.txt', "r") as output_test_file:
+          test_result=output_test_file.readlines()
+      return result, test_result
+
+class TestSortProgramm(unittest.TestCase):
+
+  def tearDown(self):
+      os.remove('output.txt')
+      os.remove('input.txt')
+
+  def test_input1(self):
+      result, test_result = runTest('1')
+      self.assertEqual(result, test_result)
+      
+  def test_input2(self):
+      result, test_result = runTest('2')
+      self.assertEqual(result, test_result)
+      
+  def test_input3(self):
+      result, test_result = runTest('3')
+      self.assertEqual(result, test_result)
+      
+if __name__ == '__main__':
+    unittest.main()
